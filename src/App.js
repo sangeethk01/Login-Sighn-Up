e0f6c20637c50a27f7d4075c74752acb74872f94
sangeethk01 <sangeethsudhakaran123@gmail.com>
2024-12-20 11:41:59 +0530

    react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginSighnup from './Components/LoginSighnup/LoginSighnup';
+
+function App() {
+  return (
+    <div >
+      <LoginSighnup/>
+    </div>
+  );
+}
+
+export default App;
